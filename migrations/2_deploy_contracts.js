1fae97b5f6d19ae2caa87c0510b2405607bae9d7
Diego Giuliani <diego.giuliani@diego.giuliani>
2021-03-12 10:55:01 -0300

    Integrate Farm token with Dapp and Dai contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,27 @@
+const DaiToken = artifacts.require("DaiToken");
+const DappToken = artifacts.require("DappToken");
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function (deployer) {
-  deployer.deploy(TokenFarm);
+
+module.exports = async function (deployer, network, accounts) {
+  // Deploy the mock DaiToken
+  await deployer.deploy(DaiToken);
+  // Read the deployed address
+  const daiToken = await DaiToken.deployed()
+
+  // Deploy the mock DappToken
+  await deployer.deploy(DappToken);
+  // Read the deployed address
+  const dappToken = await DappToken.deployed()
+
+
+  // Deploy the token farm with the corresponding arguments
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
+
+  // Transfer all dapp tokens (1 million)
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000');
+
+  // Transfer 100 mock DAI tokens to investor
+  await daiToken.transfer(accounts[1], '100000000000000000000')
 };
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,5 +1,17 @@
-pragma solidity ^0.5.4;
+pragma solidity ^0.5.0;
+import "./DappToken.sol";
+import "./DaiToken.sol";
 
 contract TokenFarm {
+    // Declare local variables
     string public name = "Dapp Token Farm";
+
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    //Executes only once when the contract is created
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
